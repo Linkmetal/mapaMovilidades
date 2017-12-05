2c36802de3986957eb86a045c9681282c0c03d6e
Linkmetal <linkmetal18@gmail.com>
2017-12-05 04:55:53 +0000

    Creado el layout para movil mediante bootstrap

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+//Clave maps : AIzaSyAww3Txk8R0wzgSvtIgf_hsgBbbFeJlxAE
+//Clave geocoding : AIzaSyDY6jBx513uHdlLA9wa-7r0KZirNLsfw6M
+
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 55.0189645, lng: 15.22315479 },
+    zoom: 3
+  });
+}
